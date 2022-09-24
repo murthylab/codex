bc83e1d6266231110803ec1c995047a02921859f
Arie Matsliah <arie.matsliah@gmail.com>
2022-09-24 09:14:52 -0400

    Update cloudrun endpoint

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ gcloud run deploy --source . \
         --min-instances 1 \
         --cpu 4 \
         --memory 4G \
-        flywire-index && \
+        code && \
 echo "Running post deploy smoke tests. Make sure env variable SMOKE_TEST_PROD_KEY is set to match the value stored in secret manager (GCP) in order to bypass auth" && \
 for i in $(seq 1 5); do curl "https://code.pniapps.org/app/search?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Cell ID" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
 for i in $(seq 1 5); do curl "https://code.pniapps.org/app/explore?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Classes" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
