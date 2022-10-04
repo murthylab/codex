1fd824b79dbf6ec02d106d84ae06f3bff8507ee7
Arie Matsliah <arie.matsliah@gmail.com>
2022-10-04 16:39:18 -0400

    Do not match IDs

--- src/data/search_index.py
+++ src/data/search_index.py
@@ -154,7 +154,7 @@ class SearchIndex(object):
 
     def closest_token(self, term, case_sensitive, limited_ids_set=None):
         term = term.strip()
-        if not term:
+        if not term or term.isnumeric():  # do not suggest number/id close matches
             return None
         if case_sensitive:
             indx = self.CS_token_to_row_id
