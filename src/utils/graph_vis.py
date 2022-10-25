344a35c439c4dfdac4fff3eb8dcf91a8029c9fc7
Kai Kuehner <kvk@pobox.com>
2022-10-25 15:20:50 -0400

    properly navigate to links

--- src/utils/graph_vis.py
+++ src/utils/graph_vis.py
@@ -31,7 +31,7 @@ def make_graph_html(connection_table, neuron_data_fetcher, center_id=None):
     def node_title(nd):
         rid = nd['root_id']
         name = nd['name']
-        return f"<a href=\"cell_details?root_id={rid}\">{name}</a>"
+        return f"<a href='cell_details?root_id={rid}' target='_parent'>{name}</a>"
 
     def edge_title(row):
         return f'{row[2]}'
