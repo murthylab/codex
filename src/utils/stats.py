4968b0ff23b06d0a357541444e5b12f79dcc5741
Kai Kuehner <kvk@pobox.com>
2022-10-28 18:21:23 -0400

    split up input and output charts

--- src/utils/stats.py
+++ src/utils/stats.py
@@ -12,14 +12,15 @@ def make_donut_chart_from_list(key_title, val_title, item_list):
 def _make_data_charts(data_list):
     nt_types = []
     input_output_regions = []
-    input_output_neuropils = []
+    input_neuropils = []
+    output_neuropils = []
     classes = []
     unknown_key = "Unknown"
     for d in data_list:
         nt_types.append(d['nt_type'] or unknown_key)
         input_output_regions.append(d['hemisphere_fingerprint'] or unknown_key)
-        input_output_neuropils.extend(d['input_neuropils'] or [unknown_key])
-        input_output_neuropils.extend(d['output_neuropils'] or [unknown_key])
+        input_neuropils.extend(d['input_neuropils'] or [unknown_key])
+        output_neuropils.extend(d['output_neuropils'] or [unknown_key])
         classes.append(str(len(d['classes']) if d['classes'] else 0))
 
     result = {}
@@ -29,12 +30,15 @@ def _make_data_charts(data_list):
     if input_output_regions:
         result['Input/Output hemispheres'] = make_donut_chart_from_list(
             key_title="Output regions", val_title="Count", item_list=input_output_regions)
-    if input_output_neuropils:
-        result['Input/Output neuropils'] = make_donut_chart_from_list(
-            key_title="Output neuropils", val_title="Count", item_list=input_output_neuropils)
     if classes:
         result['Num. Assigned Neuron Classes'] = make_donut_chart_from_list(
             key_title="Num Classes", val_title="Count", item_list=classes)
+    if input_neuropils:
+        result['Input neuropils'] = make_donut_chart_from_list(
+            key_title="Input neuropils", val_title="Count", item_list=input_neuropils)
+    if output_neuropils:
+        result['Output neuropils'] = make_donut_chart_from_list(
+            key_title="Input neuropils", val_title="Count", item_list=output_neuropils)
 
     return result
 
