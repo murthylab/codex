b8039d0b2937a167fe56c4f42e38bc9117687ed6
Arie Matsliah <arie.matsliah@gmail.com>
2022-11-04 09:05:13 -0400

    present syn counts + tooltips

--- src/data/sorting.py
+++ src/data/sorting.py
@@ -40,10 +40,20 @@ def sort_search_results(query, ids, sort_by=None):
             sort_by_target_cell_rid = int(parts[1])
             con_table = load_connection_table_for_root_id(sort_by_target_cell_rid)
             if parts[0] == DOWNSTREAM_SYNAPSE_COUNT:
+                extra_data_title = (
+                    f"Number of input synapses from {sort_by_target_cell_rid}"
+                )
                 dct = {r[1]: r[3] for r in con_table if r[0] == sort_by_target_cell_rid}
             else:
+                extra_data_title = (
+                    f"Number of output synapses to {sort_by_target_cell_rid}"
+                )
                 dct = {r[0]: r[3] for r in con_table if r[1] == sort_by_target_cell_rid}
-            extra_data = ("Syn", dct)
+            extra_data = {
+                "title": extra_data_title,
+                "column_name": "Syn",
+                "values_dict": dct,
+            }
             ids = sorted(ids, key=lambda x: -dct[x])
             return ids, extra_data
     except Exception as e:
