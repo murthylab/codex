9416a6214604ab50f37f6acfe4b3e1ac37fb9b3c
Arie Matsliah <arie.matsliah@gmail.com>
2022-11-09 14:31:26 -0500

    Parse coordinates

--- src/utils/prm.py
+++ src/utils/prm.py
@@ -11,6 +11,12 @@ CELL_IDENTIFICATION_SUBMISSION_URL_TEMPLATE = (
 
 
 def cell_identification_url(cell_id, user_id, coordinates, annotation):
+    annotation = annotation or ''
+    coordinates = coordinates or ''
+    if coordinates:
+        coordinates = coordinates.replace('[', '')
+        coordinates = coordinates.replace(']', '')
+        coordinates = ','.join([p for p in coordinates.split() if p])
     return CELL_IDENTIFICATION_SUBMISSION_URL_TEMPLATE.format(
         cell_id=cell_id, user_id=user_id, coordinates=coordinates, annotation=annotation
     )
