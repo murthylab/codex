850440e52d31246dc5d3814b6d18c8060bc2b63e
Ryan Morey <4590343+rmorey@users.noreply.github.com>
2022-11-09 16:10:08 -0500

    fix run_dev on linux (#13)
    Co-authored-by: Ryan Morey <rm5876@princeton.edu>

--- run_dev.sh
+++ run_dev.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env sh
+
 export FLASK_APP=src.main && \
 export FLASK_SECRET_KEY=dev_dummy_key && \
 export SMOKE_TEST_KEY=dev_smoke_test_key && \
 export APP_ENVIRONMENT=DEV && \
-export BUILD_GIT_SHA=`git log | head -1 | cut -d " " -f 2` && \
-export BUILD_TIMESTAMP=`date` &&\
-flask run
+BUILD_GIT_SHA="$(git log | head -1 | cut -d " " -f 2)" && \
+export BUILD_GIT_SHA && \
+BUILD_TIMESTAMP="$(date)" &&\
+export BUILD_TIMESTAMP && \
+flask run	
