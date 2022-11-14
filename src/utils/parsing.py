84870b46ceaeb7c1668f4982bb296573ed0e38e6
Ryan Morey <4590343+rmorey@users.noreply.github.com>
2022-11-14 11:55:17 -0500

    Differentiate strong/weak search results (#44)
    * embolden exact matches
    
    * colorize matches
    
    * cleanup
    
    * tweaks, bold, refactor
    
    * clean
    
    * clean
    
    * fix span insertion
    
    * improve appearance, refactor
    
    * highlight all matches
    
    * new highlighting rules
    
    * refactor, add tests
    
    * more tests
    
    * pr feedback
    
    * pr feedback
    
    * pr feedback
    
    * fix tests
    
    Co-authored-by: Ryan Morey <rm5876@princeton.edu>

--- src/utils/parsing.py
+++ src/utils/parsing.py
@@ -1,4 +1,21 @@
-DELIMS = ["=", "-", ". ", ",", "?", "!", ";", ":" "//", "/", "(", ")", '"', "&"]
+DELIMS = ["=", "-", ". ", ",", "?", "!", ";", ":", "//", "/", "(", ")", '"', "&"]
+HIGHLIGHTING_DELIMS = [
+    "=",
+    "-",
+    ".",
+    ",",
+    "?",
+    "!",
+    ";",
+    ":",
+    "//",
+    "/",
+    "(",
+    ")",
+    '"',
+    "&",
+    "_",
+]
 
 
 def tokenize(s):
@@ -12,3 +29,27 @@ def tokenize(s):
 
     res = [clean(t) for t in s.split() if t]
     return [t for t in res if t]
+
+
+def tokenize_and_fold_for_highlight(s):
+    tokens = []
+
+    for d in HIGHLIGHTING_DELIMS:
+        s = s.replace(d, " ")
+
+    i = 0
+    length = len(s)
+    while i < length:
+        c = s[i]
+        if c == " ":
+            i += 1
+            continue
+        start = i
+        while i < length and s[i] != " ":
+            i += 1
+        end = i
+        token = s[start:end].casefold()
+        if token:
+            tokens.append((token, start, end))
+
+    return tokens
