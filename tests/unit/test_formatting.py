7da72982ce9294bf35332c0ab671470cb92a35bf
Ryan Morey <ryan.t.morey@gmail.com>
2022-11-14 11:38:53 -0500

    pr feedback

--- tests/unit/test_formatting.py
+++ tests/unit/test_formatting.py
@@ -5,21 +5,45 @@ from src.utils.formatting import highlight_annotations
 
 class TestHighlighting(TestCase):
     def test_tokenize_for_highlight(self):
-        tags = [
-            "Mushroom Body Kenyon cell KC",
-            "name from DOI: 10.7554/eLife.66039",
-            "ventral unpaired medial/VUM?",
+        cases = [
+            (
+                "Mushroom Body Kenyon cell KC",
+                [
+                    ("mushroom", 0, 8),
+                    ("body", 9, 13),
+                    ("kenyon", 14, 20),
+                    ("cell", 21, 25),
+                    ("kc", 26, 28),
+                ],
+            ),
+            (
+                "name from DOI: 10.7554/eLife.66039",
+                [
+                    ("name", 0, 4),
+                    ("from", 5, 9),
+                    ("doi", 10, 13),
+                    ("10", 15, 17),
+                    ("7554", 18, 22),
+                    ("elife", 23, 28),
+                    ("66039", 29, 34),
+                ],
+            ),
+            (
+                "ventral unpaired medial/VUM?",
+                [
+                    ("ventral", 0, 7),
+                    ("unpaired", 8, 16),
+                    ("medial", 17, 23),
+                    ("vum", 24, 27),
+                ],
+            ),
         ]
-        for tag in tags:
-            tokens = tokenize_and_fold_for_highlight(tag)
-            self.assertGreater(len(tokens), 0)
-
-            for token, start, end in tokens:
-                self.assertGreaterEqual(start, 0)
-                self.assertGreaterEqual(end, 0)
-                self.assertGreater(end, start)
-                self.assertGreater(len(token), 0)
-                self.assertEqual(token, tag[start:end].casefold())
+        for case in cases:
+            tag = case[0]
+            expected = case[1]
+            actual = tokenize_and_fold_for_highlight(tag)
+            self.assertGreater(len(actual), 0)
+            self.assertEqual(actual, expected)
 
     def test_highlight_annotations(self):
         input = [  # (filter_string, tags, expected)
