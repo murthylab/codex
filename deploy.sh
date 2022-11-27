caead64273b896cd0d0e948df4901beca1e3fbf2
Arie Matsliah <arie.matsliah@gmail.com>
2022-11-27 16:03:34 -0500

    smoke tests

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ TIMESTAMP=$(date)
 GIT_SHA=$(git log | head -1 | cut -d " " -f 2)
 
 pip install pytest && \
-python3 -m pytest tests/integration && \
-python3 -m pytest tests/unit && \
+python3 -m pytest tests/integration -x && \
+python3 -m pytest tests/unit -x && \
 gcloud run deploy --source . \
         --set-env-vars "APP_ENVIRONMENT=PROD" \
         --set-env-vars "BUILD_GIT_SHA=${GIT_SHA}" \
@@ -15,8 +15,10 @@ gcloud run deploy --source . \
         --memory 4G \
         codex && \
 echo "Running post deploy smoke tests. Make sure env variable SMOKE_TEST_PROD_KEY is set to match the value stored in secret manager (GCP) in order to bypass auth" && \
-for i in $(seq 1 5); do curl "https://codex.flywire.ai/app/search?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Class" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
-for i in $(seq 1 5); do curl "https://codex.flywire.ai/app/explore?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Class" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
-for i in $(seq 1 5); do curl "https://codex.flywire.ai/app/stats?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Cells" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done
-for i in $(seq 1 5); do curl "https://codex.flywire.ai/app/path_length?with_sample_input=1&smoke_test=$SMOKE_TEST_PROD_KEY" | grep "from" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done
-for i in $(seq 1 5); do curl "https://codex.flywire.ai/app/nblast?with_sample_input=1&smoke_test=$SMOKE_TEST_PROD_KEY" | grep "from" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done
+for i in $(seq 1 3); do curl "https://codex.flywire.ai/app/search?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Class" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
+for i in $(seq 1 3); do curl "https://codex.flywire.ai/app/search?filter_string=rr&download=1&smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Class" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
+for i in $(seq 1 3); do curl "https://codex.flywire.ai/app/explore?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Class" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
+for i in $(seq 1 3); do curl "https://codex.flywire.ai/app/stats?smoke_test=$SMOKE_TEST_PROD_KEY" | grep "Ascending" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
+for i in $(seq 1 3); do curl "https://codex.flywire.ai/app/path_length?with_sample_input=1&smoke_test=$SMOKE_TEST_PROD_KEY" | grep "from" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
+for i in $(seq 1 3); do curl "https://codex.flywire.ai/app/nblast?with_sample_input=1&smoke_test=$SMOKE_TEST_PROD_KEY" | grep "from" || echo -e "\033[1;31m FAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED\nFAILED  \033[0m"; done && \
+echo "Done."
