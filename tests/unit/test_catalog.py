8a94661f3feee1d3eafe244c14afd64a5364235f
Arie Matsliah <arie.matsliah@gmail.com>
2022-12-10 13:47:18 -0500

    add unit test

---
+++ tests/unit/test_catalog.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from unittest import TestCase
+from src.data.catalog import _CODEX_DATA_SCHEMA
+
+
+class CatalogTest(TestCase):
+    def test_column_intersections(self):
+        col_counts = defaultdict(int)
+        for cols in _CODEX_DATA_SCHEMA.values():
+            for c in cols:
+                col_counts[c] += 1
+        for k, v in col_counts.items():
+            if (
+                k == "root_id"
+            ):  # apart from connections file, all should contain a root_id col as key
+                self.assertEqual(len(_CODEX_DATA_SCHEMA) - 1, v)
+            elif k in ["nt_type", "position", "supervoxel_id"]:
+                self.assertEqual(2, v)
+            else:
+                self.assertEqual(1, v, k)
