fa94aba5109d82967e89fcbf0225286276292535
Arie Matsliah <arie.matsliah@gmail.com>
2022-12-12 09:44:29 -0500

    - tests for  v526 - get read of cloud distance loader

--- tests/integration/test_neuron_data.py
+++ tests/integration/test_neuron_data.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.data.local_data_loader import unpickle_all_neuron_db_versions, load_neuron_db
+from src.data.local_data_loader import unpickle_all_neuron_db_versions, load_neuron_db, unpickle_neuron_db
 from src.data.neuron_data_factory import NeuronDataFactory
 
 from src.data.versions import DATA_SNAPSHOT_VERSIONS
@@ -13,26 +13,17 @@ class NeuronDataTest(TestCase):
         # this allows to temporarily disable some checks when updating the schema of data files. should be kept empty
         # after the updates are tested and complete
         self.exclude_keys = set()
-        self.neuron_dbs = unpickle_all_neuron_db_versions(
-            data_root_path=TEST_DATA_ROOT_PATH
-        )
 
     def test_data_load_equals_pickled_db(self):
-        versions = DATA_SNAPSHOT_VERSIONS
-        loaded_neuron_dbs = {
-            v: load_neuron_db(version=v, data_root_path=TEST_DATA_ROOT_PATH)
-            for v in versions
-        }
-
         def isnan(vl):
             return vl != vl
 
-        def compare_neuron_dbs(obj, tested, golden):
-            obj.assertIsNotNone(tested)
-            obj.assertEqual(
+        def compare_neuron_dbs(tested, golden):
+            self.assertIsNotNone(tested)
+            self.assertEqual(
                 set(tested.neuron_data.keys()), set(tested.search_index.all_doc_ids())
             )
-            obj.assertEqual(
+            self.assertEqual(
                 set(tested.neuron_data.keys()), set(golden.neuron_data.keys())
             )
 
@@ -41,12 +32,12 @@ class NeuronDataTest(TestCase):
             diff_rids = set()
             for rid, nd in tested.neuron_data.items():
                 ndp = golden.get_neuron_data(rid)
-                obj.assertEqual(
-                    set(nd.keys()) - obj.exclude_keys,
-                    set(ndp.keys()) - obj.exclude_keys,
+                self.assertEqual(
+                    set(nd.keys()) - self.exclude_keys,
+                    set(ndp.keys()) - self.exclude_keys,
                 )
                 for k, val in nd.items():
-                    if k in obj.exclude_keys:
+                    if k in self.exclude_keys:
                         continue
                     if isnan(val):
                         if not isnan(ndp[k]):
@@ -57,40 +48,44 @@ class NeuronDataTest(TestCase):
                             diff_keys[k] += 1
                             diff_vals[f"{k}: {val} vs {ndp[k]}"] += 1
                             diff_rids.add(rid)
-            print(f"Diff RIDs {len(diff_rids)}: {sorted(diff_rids)[:100]}")
-            obj.assertEqual(
+            if diff_rids:
+                print(f"Diff RIDs {len(diff_rids)}: {sorted(diff_rids)[:100]}")
+            self.assertEqual(
                 0,
                 len(diff_keys),
-                f"Diff keys not empty: {diff_keys}\n\n {len(diff_vals)}\n\n {diff_vals=}",
+                f"Diff keys not empty: {diff_keys=}\n\n {len(diff_vals)=}\n\n {diff_vals=}"[:1000],
             )
             # compare optional output sets (adjacency)
-            obj.assertEqual(len(tested.connection_rows), len(golden.connection_rows))
+            self.assertEqual(len(tested.connection_rows), len(golden.connection_rows))
             for r1, r2 in zip(
                 sorted(tested.connection_rows), sorted(golden.connection_rows)
             ):
-                obj.assertEqual(r1, r2)
+                self.assertEqual(r1, r2)
             if tested.input_sets():
                 connected_cells = set(tested.input_sets().keys()).union(
                     set(tested.output_sets().keys())
                 )
                 not_connected_cells = set(tested.neuron_data.keys()) - connected_cells
-                obj.assertGreater(2000, len(not_connected_cells))
+                self.assertGreater(2000, len(not_connected_cells))
 
             # compare optional label data
-            obj.assertEqual(tested.label_data, golden.label_data)
+            self.assertEqual(tested.label_data, golden.label_data)
 
         # check that all versions loaded
-        for v in versions:
-            compare_neuron_dbs(
-                self, tested=loaded_neuron_dbs[v], golden=self.neuron_dbs[v]
-            )
+        for v in DATA_SNAPSHOT_VERSIONS:
+            print(f"Testing version {v}...")
+            unpickled_db = unpickle_neuron_db(version=v, data_root_path=TEST_DATA_ROOT_PATH)
+            loaded_db = load_neuron_db(version=v, data_root_path=TEST_DATA_ROOT_PATH)
+            compare_neuron_dbs(tested=loaded_db, golden=unpickled_db)
+            del loaded_db
 
-        # check the same for data factory
-        neuron_data_factory = NeuronDataFactory(data_root_path=TEST_DATA_ROOT_PATH)
-        for v in versions:
-            compare_neuron_dbs(
-                self, tested=neuron_data_factory.get(v), golden=self.neuron_dbs[v]
-            )
+            # check the same for data factory
+            neuron_data_factory = NeuronDataFactory(data_root_path=TEST_DATA_ROOT_PATH, preload_latest=False)
+            factory_db = neuron_data_factory.get(v)
+            compare_neuron_dbs(tested=factory_db, golden=unpickled_db)
+            del unpickled_db
+            del factory_db
+            print(f"Done testing version {v}")
 
     # this is a helper test to not forget clean up excluded keys once schema has updated
     def test_no_keys_excluded(self):
