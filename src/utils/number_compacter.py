4ec2e5e03da5826098062844a1e0ccd612faf791
Arie Matsliah <arie.matsliah@gmail.com>
2023-01-16 13:09:16 -0500

    number compacter

---
+++ src/utils/number_compacter.py
@@ -0,0 +1,24 @@
+import string
+
+CHARS = string.digits + string.ascii_uppercase + string.ascii_lowercase
+CHARSLEN = len(CHARS)
+
+def number_to_compact_repr(num):
+    if num < 0:
+        return f"-{number_to_compact_repr(-num)}"
+    assert isinstance(num, int)
+    s = ""
+    while num:
+        s = CHARS[num % CHARSLEN] + s
+        num //= CHARSLEN
+    return s or CHARS[0]
+
+def compact_repr_to_number(reprStr):
+    if reprStr.startswith("-"):
+        return -compact_repr_to_number(reprStr[1:])
+    assert isinstance(reprStr, str) and all(c in CHARS for c in reprStr)
+    num = 0
+    for i, c in enumerate(reversed(reprStr)):
+        num += CHARS.index(c) * (CHARSLEN ** i)
+    return num
+
---
+++ tests/unit/test_number_compacter.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from src.utils.number_compacter import number_to_compact_repr, compact_repr_to_number
+
+TEST_CASES = {
+    0: "0",
+    1: "1",
+    10: "A",
+    36: "a",
+    62: "10",
+    3843: "zz",
+    238327: "zzz",
+    14776335: "zzzz",
+}
+TEST_CASES.update({-k: f"-{v}" for k, v in TEST_CASES.items() if k})
+
+class CompacterTest(TestCase):
+    def test_number_to_compact_repr(self):
+        for k, v in TEST_CASES.items():
+            self.assertEqual(v, number_to_compact_repr(k))
+
+    def test_compact_repr_to_number(self):
+        for k, v in TEST_CASES.items():
+            self.assertEqual(k, compact_repr_to_number(v))
+
+    def test_equivalence(self):
+        for i in range(30):
+            num = pow(3, i)
+            self.assertEqual(num, compact_repr_to_number(number_to_compact_repr(num)))
+            self.assertEqual(-num, compact_repr_to_number(number_to_compact_repr(-num)))
