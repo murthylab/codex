3b869b916cf041477fe7e13c1c1e299482c31328
Ryan Morey <ryan.t.morey@gmail.com>
2023-01-19 11:09:54 -0500

    pre-commit

--- src/data/genetic_lines.py
+++ src/data/genetic_lines.py
@@ -1,13 +1,16 @@
 from src.data.gcs_data_loader import load_jennet_lines_from_gcs
 from src.data.brain_regions import REGIONS, REGION_CATEGORIES
 
+
 def add_neuropils_to_jennet_lines(lines):
     for compartments in lines.values():
         for compartment, data in compartments.items():
             if compartment in REGIONS:
                 data["neuropils"] = [compartment]
 
-            elif lr := [n for n in [f"{compartment}_L", f"{compartment}_R"] if n in REGIONS]:
+            elif lr := [
+                n for n in [f"{compartment}_L", f"{compartment}_R"] if n in REGIONS
+            ]:
                 data["neuropils"] = lr
 
             elif substr := [n for n in REGIONS if compartment in n]:
@@ -16,7 +19,7 @@ def add_neuropils_to_jennet_lines(lines):
             elif compartment == "OL":
                 data["neuropils"] = REGION_CATEGORIES["optic lobe"]
 
-            elif compartment == "SEG": # "subesophageal ganglion" -> “gnathal ganglia”
+            elif compartment == "SEG":  # "subesophageal ganglion" -> “gnathal ganglia”
                 data["neuropils"] = ["GNG"]
 
     return lines
