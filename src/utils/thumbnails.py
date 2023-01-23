2afd48c87490d3e3ea4e0848b626b3e612ffa45c
Arie Matsliah <arie.matsliah@gmail.com>
2023-01-23 12:15:42 -0500

    wip

--- src/utils/thumbnails.py
+++ src/utils/thumbnails.py
@@ -1,3 +1,16 @@
+import requests
+
 # 'cell_or_neuropil' is either a root id, or neuropil abbreviation (upper case, e.g. GNG)
-def url_for_skeleton(cell_or_neuropil):
-    return f"https://storage.googleapis.com/flywire-data/codex/skeleton_thumbnails/{cell_or_neuropil}.png"
+def url_for_skeleton(cell_or_neuropil, animated=True):
+    gif_folder = 'skeleton_gifs'
+    jpg_folder = 'skeleton_thumbnails'
+
+    def build_url(folder, file, ext):
+        return f"https://storage.googleapis.com/flywire-data/codex/{folder}/{file}.{ext}"
+
+    url = build_url(gif_folder, cell_or_neuropil, 'gif')
+    r = requests.head(url)
+    print(f"+++ {r.headers['Content-Type']}")
+
+    return url if 'image' in r.headers['Content-Type'] else build_url(jpg_folder, cell_or_neuropil, 'png')
+
