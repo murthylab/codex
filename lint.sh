be834720ad4f8f672b8baac5991291744dc73422
Ryan Morey <ryan.t.morey@gmail.com>
2023-02-13 09:10:37 -0500

    tidy

--- lint.sh
+++ lint.sh
@@ -1,2 +1,2 @@
-python3 -m pip install -r requirements_dev.txt
+# python3 -m pip install -r requirements_dev.txt
 python3 -m black . && python3 -m ruff .
--- run_local_dev.sh
+++ run_local_dev.sh
@@ -1,2 +1,2 @@
+#!/usr/bin/env sh
 export FLASK_ENV=development && ./run_local.sh
-
