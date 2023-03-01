ea760dace0451db21b22d1ecec7f8bccc079ad44
Arie Matsliah <arie.matsliah@gmail.com>
2023-03-01 12:02:20 -0500

    Tweak FAQ

--- src/data/faq_qa_kb.py
+++ src/data/faq_qa_kb.py
@@ -27,8 +27,7 @@ FAQ_QA_KB = {
     "data_version": {
         "q": "What version of FlyWire data is shown?",
         "a": "<b>Codex</b> provides access to static FlyWire connectome data releases (sometimes referred to as"
-        " 'snapshots' or 'versions'). The current most-recent snapshot release is from Dec 2022, and this "
-        "is the one served by default. To switch from the most recent release to one of the older releases use "
+        " 'snapshots' or 'versions'). To switch from the default data snapshot one of the older releases use "
         "the <b>Data Version</b> selector under the search settings menu. And for live queries against the "
         "proofreading database tables use the "
         '<a href="https://github.com/seung-lab/CAVEclient">Cave Client Python library</a> instead.',
