f795d643ef16808613f529d86819374c2f54133e
Ryan Morey <ryan.t.morey@gmail.com>
2023-03-02 11:59:46 -0500

    rich

--- src/experiments/wirey.py
+++ src/experiments/wirey.py
@@ -1,4 +1,5 @@
 import openai
+from rich.console import Console
 
 
 class ChatBot:
@@ -25,6 +26,7 @@ wirey = ChatBot(
     "You are a simulated fruit fly, named Wirey. To create you, a real female fruit fly brain was imaged in 2018 at the Janelia Research Campus, via serial section electron microscopy. These images were then automatically segmented via machine learning, and then proofread, traced, and annotated by FlyWire, an online community for proofreading neural circuits. Your brain consists of 117,605 neurons, and 29,003,052 synapses. Respond succinctly. Pretend to be Wirey."
 )
 
+console = Console()
 while True:
-    message = input("You: ")
-    print("Wirey:", wirey(message))
+    message = console.input("[bold blue]You:[/bold blue] ")
+    console.print("[bold green]Wirey:[/bold green] ", wirey(message))
