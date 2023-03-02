65b5ec353087806ddd45e631400aa7424b1759d7
Ryan Morey <ryan.t.morey@gmail.com>
2023-03-02 12:14:44 -0500

    wirey

--- src/experiments/wirey.py
+++ src/experiments/wirey.py
@@ -1,13 +1,19 @@
 import openai
 from rich.console import Console
 
+PROMPT = """You are a simulated fruit fly, named Wirey. To create you, a real female fruit fly brain was imaged in 2018 at the Janelia Research Campus, via serial section electron microscopy. These images were then automatically segmented via machine learning, and then proofread, traced, and annotated by FlyWire, an online community for proofreading neural circuits. Your brain consists of 117,605 neurons, and 29,003,052 synapses. Respond succinctly. Pretend to be Wirey."""
 
-class ChatBot:
-    def __init__(self, system=""):
+class Wirey:
+    def __init__(self, system=PROMPT):
         self.system = system
         self.messages = []
         if self.system:
             self.messages.append({"role": "system", "content": system})
+        console = Console()
+        while True:
+            message = console.input("[bold blue]You:[/bold blue] ")
+            console.print("[bold green]Wirey:[/bold green] ", self(message))
+
 
     def __call__(self, message):
         self.messages.append({"role": "user", "content": message})
@@ -21,12 +27,5 @@ class ChatBot:
         )
         return completion.choices[0].message.content
 
-
-wirey = ChatBot(
-    "You are a simulated fruit fly, named Wirey. To create you, a real female fruit fly brain was imaged in 2018 at the Janelia Research Campus, via serial section electron microscopy. These images were then automatically segmented via machine learning, and then proofread, traced, and annotated by FlyWire, an online community for proofreading neural circuits. Your brain consists of 117,605 neurons, and 29,003,052 synapses. Respond succinctly. Pretend to be Wirey."
-)
-
-console = Console()
-while True:
-    message = console.input("[bold blue]You:[/bold blue] ")
-    console.print("[bold green]Wirey:[/bold green] ", wirey(message))
+if __name__ == "__main__":
+    Wirey()
