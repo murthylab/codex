c22a969adce78642f7dfd634d987b48bd69cbeb2
Kai Kuehner <kvk@pobox.com>
2023-04-11 15:21:22 -0400

    individually require source and target cells; make MAX_NEURONS_FOR_DOWNLOAD apply individually

--- src/blueprints/app.py
+++ src/blueprints/app.py
@@ -614,47 +614,54 @@ def path_length():
     )
     message = None
 
-    if source_cell_names_or_ids or target_cell_names_or_ids:
-        neuron_db = NeuronDataFactory.instance().get()
-        root_ids_src = set()
-        root_ids_target = set()
-        if source_cell_names_or_ids:
-            root_ids_src = set(neuron_db.search(search_query=source_cell_names_or_ids))
-        if target_cell_names_or_ids:
-            root_ids_target = set(
-                neuron_db.search(search_query=target_cell_names_or_ids)
-            )
-        root_ids_src = sorted(root_ids_src)
-        root_ids_target = sorted(root_ids_target)
-        if not root_ids_src or not root_ids_target:
-            return render_error(
-                title="No matching cells found",
-                message=f"Could not find any cells matching '{source_cell_names_or_ids} -> {target_cell_names_or_ids}'",
-            )
-        elif len(root_ids_src) + len(root_ids_target) > MAX_NEURONS_FOR_DOWNLOAD:
-            too_long_len = len(root_ids_src) + len(root_ids_target)
-            root_ids_src = root_ids_src[: MAX_NEURONS_FOR_DOWNLOAD // 2]
-            root_ids_target = root_ids_target[: MAX_NEURONS_FOR_DOWNLOAD // 2]
-            message = (
-                f"{too_long_len} cells match your query. "
-                f"Fetching pathways for the first {len(root_ids_src) + len(root_ids_target)} matches."
-            )
+    if not source_cell_names_or_ids or not target_cell_names_or_ids:
+        return render_error(
+            title="No cells provided",
+            message=f"Both source and target cells must be specified.",
+        )
+    neuron_db = NeuronDataFactory.instance().get()
+    root_ids_src = set()
+    root_ids_target = set()
+    root_ids_src = set(neuron_db.search(search_query=source_cell_names_or_ids))
+    root_ids_target = set(neuron_db.search(search_query=target_cell_names_or_ids))
+    root_ids_src = sorted(root_ids_src)
+    root_ids_target = sorted(root_ids_target)
+    if not root_ids_src:
+        return render_error(
+            title="No matching cells found",
+            message=f"Could not find any cells matching '{source_cell_names_or_ids}'.",
+        )
+    if not root_ids_target:
+        return render_error(
+            title="No matching cells found",
+            message=f"Could not find any cells matching '{target_cell_names_or_ids}'.",
+        )
+    if len(root_ids_src) > MAX_NEURONS_FOR_DOWNLOAD:
+        message = (
+            f"{len(root_ids_src)} source cells match your query. "
+            f"Fetching pathways for the first {MAX_NEURONS_FOR_DOWNLOAD} matches."
+        )
+        root_ids_src = root_ids_src[:MAX_NEURONS_FOR_DOWNLOAD]
+    if len(root_ids_target) > MAX_NEURONS_FOR_DOWNLOAD:
+        message = (
+            f"{len(root_ids_target)} target cells match your query. "
+            f"Fetching pathways for the first {MAX_NEURONS_FOR_DOWNLOAD} matches."
+        )
+        root_ids_target = root_ids_target[:MAX_NEURONS_FOR_DOWNLOAD]
 
-        matrix = distance_matrix(
-            sources=root_ids_src,
-            targets=root_ids_target,
-            neuron_db=neuron_db,
-            min_syn_count=min_syn_count,
+    matrix = distance_matrix(
+        sources=root_ids_src,
+        targets=root_ids_target,
+        neuron_db=neuron_db,
+        min_syn_count=min_syn_count,
+    )
+    if len(matrix) <= 1:
+        return render_error(
+            f"Path lengths for Cell IDs {root_ids_src} -> {root_ids_target} are not available."
         )
-        if len(matrix) <= 1:
-            return render_error(
-                f"Path lengths for Cell IDs {root_ids_src} -> {root_ids_target} are not available."
-            )
-        log_activity(
-            f"Generated path lengths table for {root_ids_src} -> {root_ids_target} {download=}"
-        )
-    else:
-        matrix = []
+    log_activity(
+        f"Generated path lengths table for {root_ids_src} -> {root_ids_target} {download=}"
+    )
 
     if download:
         fname = "path_lengths.csv"
