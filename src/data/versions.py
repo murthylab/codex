df583dd5283705b246f4e7402e1ebb0d4b9ed623
Arie Matsliah <arie.matsliah@gmail.com>
2023-04-12 17:16:01 -0400

    Make v630 default. Get rid of v526

--- src/data/versions.py
+++ src/data/versions.py
@@ -1,9 +1,8 @@
 DATA_SNAPSHOT_VERSION_DESCRIPTIONS = {
-    "526": "Snapshot 526 - Dec 2022",
-    "571": "Snapshot 571 - Jan 2023 [default]",
-    "630": "Snapshot 630 - Apr 2023 [coming soon]",
+    "571": "Snapshot 571 - Jan 2023",
+    "630": "Snapshot 630 - Apr 2023 [default]",
 }
 
 DATA_SNAPSHOT_VERSIONS = sorted(DATA_SNAPSHOT_VERSION_DESCRIPTIONS.keys(), reverse=True)
 
-DEFAULT_DATA_SNAPSHOT_VERSION = "571"
+DEFAULT_DATA_SNAPSHOT_VERSION = "630"
