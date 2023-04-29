7278c9d502173d002d8cc1a44bfa27afd7eaad96
Arie Matsliah <arie.matsliah@gmail.com>
2023-04-29 12:15:54 -0400

    rename

--- src/blueprints/base.py
+++ src/blueprints/base.py
@@ -248,7 +248,7 @@ def about_codex():
 
 @base.route("/about_flywire", methods=["GET", "POST"])
 @request_wrapper
-def about_flywire_connectome():
+def about_flywire():
     log_activity("Loading FlyWire Connectome page")
     return render_template(
         "about_flywire.html",
