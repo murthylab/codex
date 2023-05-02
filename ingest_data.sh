cc183fecbff305d0ce588be5daeca704bf343c95
Arie Matsliah <arie.matsliah@gmail.com>
2023-05-02 14:09:57 -0400

    Add step to ingest script

--- ingest_data.sh
+++ ingest_data.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 python3 -m src.etl.compile_data && \
 python3 -m src.etl.pickle_data && \
+python3 -m src.etl.update_download_catalog && \
 python3 -m pytest tests/unit/test_stats_utils.py -x && \
 python3 -m pytest tests/unit/test_neuron_data.py -x && \
 python3 -m pytest tests/integration -x && \
