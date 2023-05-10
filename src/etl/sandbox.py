8e3f0f748e43f0a65de95d72240edd13d61b4af5
Arie Matsliah <arie.matsliah@gmail.com>
2023-05-10 11:44:56 -0400

    Jaccard connectivity clustering

--- src/etl/sandbox.py
+++ src/etl/sandbox.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
 import random
 
-from networkx import Graph, DiGraph, connected_components, strongly_connected_components
+from networkx import DiGraph, connected_components, strongly_connected_components
 
 from src.data.local_data_loader import write_csv, unpickle_neuron_db, read_csv
 from src.data.neuron_data_factory import NeuronDataFactory
 from src.utils.formatting import percentage
+from networkx import Graph, community
 
 """
 Clusters a set of neurons by morphological similarity. Uses iterative application of the connected component analysis on
@@ -164,67 +165,106 @@ def cluster_SEZ_cells():
     )
 
 
-def generate_con_jacard_similarity_table(neuron_db):
+def generate_con_jaccard_similarity_table(neuron_db):
+    # split neurons into buckets by similarity heuristic (to reduce number of candidate pairs)
+    candidate_buckets = defaultdict(list)
+    for rid, nd in neuron_db.neuron_data.items():
+        candidate_buckets[
+            f"{nd['side']}:{nd['input_neuropils']}:{nd['output_neuropils']}:{len(str(nd['input_synapses']))}:{len(str(nd['output_synapses']))}"
+        ].append(rid)
+    num_candidate_pairs = sum(
+        [len(s) * (len(s) - 1) // 2 for s in candidate_buckets.values()]
+    )
+    print(
+        f"{len(candidate_buckets)} buckets, {num_candidate_pairs} candidates, max bucket size: {max([len(v) for v in candidate_buckets.values()])}"
+    )
+    # print out large buckets (for tuning bucketing method)
+    for k, v in candidate_buckets.items():
+        if len(v) > 3000:
+            print(f"{k}: {len(v)}")
+
+    ins, outs = neuron_db.input_output_partners_with_synapse_counts()
+
+    def jaccard(d1, d2):
+        if not d1 and not d2:
+            return 0
+        combined_key_set = set(d1.keys()).union(d2.keys())
+        min_sum = 0
+        max_sum = 0
+        for ck in combined_key_set:
+            v1 = d1.get(ck, 0)
+            v2 = d2.get(ck, 0)
+            min_sum += min(v1, v2)
+            max_sum += max(v1, v2)
+        return min_sum / max_sum
+
     s = 0
     combined_score = 0
 
-    region_projection_buckets = defaultdict(list)
-    for rid, nd in neuron_db.neuron_data.items():
-        region_projection_buckets[
-            f"{nd['side']}:{nd['input_neuropils']}:{nd['output_neuropils']}"
-        ].append(rid)
-    print(
-        f"{len(region_projection_buckets)=} {max([len(v) for v in region_projection_buckets.values()])=}"
-    )
-
-    for k, v in region_projection_buckets.items():
-        if len(v) > 5000:
-            print(f"{k}: {len(v)}")
-
-    ins, outs = neuron_db.input_output_partner_sets()
-
-    def jacard(s1, s2):
-        return len(s1.intersection(s2)) / len(s1.union(s2)) if s1 or s2 else 0
-
-    tbl = [["id1", "id2", "jacard score"]]
-    for i, rids in enumerate(region_projection_buckets.values()):
-        if i % 100 == 0:
-            print(f"{percentage(i, len(region_projection_buckets))}")
+    tbl = [["id1", "id2", "jaccard score", "ins 1", "outs 1", "ins 2", "outs 2"]]
+    for i, rids in enumerate(candidate_buckets.values()):
         for j, r1 in enumerate(rids):
             r1_ins = ins[r1]
             r1_outs = outs[r1]
             for k, r2 in enumerate(rids[j + 1 :]):
                 r2_ins = ins[r2]
                 r2_outs = outs[r2]
-                jscore = jacard(r1_ins, r2_ins) + jacard(r1_outs, r2_outs)
+                jscore = (jaccard(r1_ins, r2_ins) + jaccard(r1_outs, r2_outs)) / 2
                 combined_score += jscore
                 s += 1
-                if jscore > 0.5:
-                    tbl.append([r1, r2, jscore])
+                if s % (num_candidate_pairs // 100) == 0:
+                    print(
+                        f"scanned {s}, added {len(tbl)}, {percentage(s, num_candidate_pairs)}"
+                    )
+                if jscore > 0:
+                    tbl.append(
+                        [
+                            r1,
+                            r2,
+                            jscore,
+                            neuron_db.get_neuron_data(r1)["input_cells"],
+                            neuron_db.get_neuron_data(r1)["output_cells"],
+                            neuron_db.get_neuron_data(r2)["input_cells"],
+                            neuron_db.get_neuron_data(r2)["output_cells"],
+                        ]
+                    )
+
     print(f"{s} {combined_score}")
     write_csv(
-        filename="../../static/experimental_data/jacard_scores.csv.gz",
+        filename="static/experimental_data/jaccard_scores_weighted.csv.gz",
         rows=tbl,
         compress=True,
     )
 
 
-def analyse_con_jacard_similarities(neuron_db):
+def analyse_con_jaccard_similarities(neuron_db):
     scores_table = read_csv(
-        filename="../../static/experimental_data/jacard_scores.csv.gz"
+        filename="static/experimental_data/jaccard_scores_weighted.csv.gz"
     )
     print(f"{len(scores_table)=}")
 
     scores_dict = {}
     tot_score = 0.0
+    score_sets = defaultdict(set)
     for r in scores_table[1:]:
-        scores_dict[(int(r[0]), int(r[1]))] = float(r[2])
-        tot_score += float(r[2])
+        score = float(r[2])
+        scores_dict[(int(r[0]), int(r[1]))] = score
+        tot_score += score
+        score_sets[r[2][2]].add(r[0])
+        score_sets[r[2][2]].add(r[1])
+        if score > 0.7 and sum([int(v) for v in r[3:]]) > 10:
+            print(r)
+
+    print(
+        f"Number of neurons with any jaccard score: {len(set.union(*list(score_sets.values())))}"
+    )
+    for k, v in score_sets.items():
+        print(f"{len(v)} neurons with jaccard score ~ 0.{k}")
 
     print(f"Avg score: {tot_score / len(scores_dict)}")
 
     def measure_internal_score(attr_name):
-        def same_attr(pair):
+        def compare_attr(pair):
             nd1, nd2 = (
                 neuron_db.neuron_data[pair[0]],
                 neuron_db.neuron_data[pair[1]],
@@ -233,32 +273,24 @@ def analyse_con_jacard_similarities(neuron_db):
             if not attr1 or not attr2:
                 return None
             if isinstance(nd1[attr_name], list):
-                return len(set(nd1[attr_name]).intersection(nd2[attr_name])) > 0
+                return (
+                    "eq"
+                    if len(set(nd1[attr_name]).intersection(nd2[attr_name])) > 0
+                    else "neq"
+                )
             else:
-                return nd1[attr_name] == nd2[attr_name]
-
-        def diff_attr(pair):
-            nd1, nd2 = (
-                neuron_db.neuron_data[pair[0]],
-                neuron_db.neuron_data[pair[1]],
-            )
-            attr1, attr2 = nd1[attr_name], nd2[attr_name]
-            if not attr1 or not attr2:
-                return None
-            if isinstance(nd1[attr_name], list):
-                return len(set(nd1[attr_name]).intersection(nd2[attr_name])) == 0
-            else:
-                return nd1[attr_name] != nd2[attr_name]
+                return "eq" if nd1[attr_name] == nd2[attr_name] else "neq"
 
         same_scores = 0
         same_pairs = 0
         diff_scores = 0
         diff_pairs = 0
         for p, s in scores_dict.items():
-            if same_attr(p):
+            ca = compare_attr(p)
+            if ca == "eq":
                 same_scores += s
                 same_pairs += 1
-            elif diff_attr(p):
+            elif ca == "neq":
                 diff_scores += s
                 diff_pairs += 1
 
@@ -270,7 +302,7 @@ def analyse_con_jacard_similarities(neuron_db):
 
     measure_internal_score("cell_type")
     measure_internal_score("hemibrain_type")
-    measure_internal_score("cluster")
+    measure_internal_score("morphology_cluster")
     measure_internal_score("class")
     measure_internal_score("super_class")
     measure_internal_score("label")
@@ -279,5 +311,48 @@ def analyse_con_jacard_similarities(neuron_db):
     measure_internal_score("nerve")
 
 
+def cluster_by_jaccard_similarities():
+    scores_table = read_csv(
+        filename="static/experimental_data/jaccard_scores_weighted.csv.gz"
+    )
+    print("Building score graph..")
+    G = Graph()
+    for r in scores_table[1:]:
+        n1, n2, score = int(r[0]), int(r[1]), float(r[2])
+        G.add_node(n1)
+        G.add_node(n2)
+        G.add_edge(n1, n2, weight=100 / score)
+
+    print("Running community analysis..")
+    communities_generator = community.louvain_communities(G, resolution=200)
+    clusters_dict = {}
+    component_id = 0
+    max_xluster_size = 0
+    for s in sorted(communities_generator, key=lambda x: -len(x)):
+        if len(s) > 1:
+            max_xluster_size = max(max_xluster_size, len(s))
+            component_id += 1
+            cluster_name = f"CC_{component_id}.{len(s)}"
+            for rid in s:
+                clusters_dict[rid] = cluster_name
+
+    print(
+        f"Total clustered rids: {len(clusters_dict)}, {max_xluster_size=}, # clusters: {component_id}"
+    )
+    clusters_table = [["root_id", "connectivity_cluster"]]
+    for rid, cl in clusters_dict.items():
+        clusters_table.append([rid, cl])
+
+    print("Writing results to file..")
+    write_csv(
+        filename="static/experimental_data/connectivity_clusters.csv.gz",
+        rows=clusters_table,
+        compress=True,
+    )
+
+
 if __name__ == "__main__":
-    compare_versions("571", "630")
+    # compare_versions("571", "630")
+    # generate_con_jaccard_similarity_table(NeuronDataFactory().get())
+    # analyse_con_jaccard_similarities(NeuronDataFactory().get())
+    cluster_by_jaccard_similarities()
