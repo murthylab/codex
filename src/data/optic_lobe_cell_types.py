682a64b933011bca9ca3c102c42d5a9e1bfcfa24
Arie Matsliah <arie.matsliah@gmail.com>
2023-05-13 08:55:54 -0400

    OL tagging static data

---
+++ src/data/optic_lobe_cell_types.py
@@ -0,0 +1,40 @@
+COLUMNAR_CELL_TYPES = [
+    "C2",
+    "C3",
+    "Dm2",
+    "Dm8",
+    "L1",
+    "L2",
+    "L3",
+    "L4",
+    "L5",
+    "Mi1",
+    "Mi4",
+    "Mi9",
+    "R1-6",
+    "R7",
+    "R8",
+    "T1",
+    "T4a",
+    "T4b",
+    "T4c",
+    "T4d",
+    "T5a",
+    "T5b",
+    "T5c",
+    "T5d",
+    "Tm1",
+    "Tm2",
+    "Tm20",
+    "Tm9",
+]
+
+COLUMNAR_CELL_TYPE_TARGET_QUANTITIES_LR = {
+    t: {
+        "left": 790,
+        "right": 780,
+    }
+    for t in COLUMNAR_CELL_TYPES
+}
+# R1-6 is a lump of 6 indistinguishable types
+COLUMNAR_CELL_TYPE_TARGET_QUANTITIES_LR["R1-6"] = {"left": 6 * 790, "right": 6 * 780}
