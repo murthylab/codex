7dba20c50298948b338e1c623a40aab1ab3659e3
Arie Matsliah <arie.matsliah@gmail.com>
2023-07-15 08:19:47 -0400

    motif search backend (wip)

--- src/service/motif_search.py
+++ src/service/motif_search.py
@@ -9,7 +9,7 @@ from src.data.versions import DEFAULT_DATA_SNAPSHOT_VERSION
 from src.data.neuron_data import NeuronDB
 
 MAX_NODES = 3
-DEFAULT_LIMIT = 1000
+DEFAULT_LIMIT = 10
 
 EdgeConstraints = namedtuple("EdgeConstraints", "regions min_synapse_count nt_type")
 
@@ -429,6 +429,127 @@ class MotifSearchQuery(object):
         return matches
 
     @staticmethod
+    def row_satisfies_constraints(row, edge_constraints):
+        if (
+            edge_constraints.min_synapse_count
+            and row[3] < edge_constraints.min_synapse_count
+        ):
+            return False
+        if edge_constraints.regions and row[2] not in edge_constraints.regions:
+            return False
+        if edge_constraints.nt_type and row[4] != edge_constraints.nt_type:
+            return False
+        return True
+
+    @staticmethod
+    def _feasible_pairs(
+        neuron_db, x_candidates, y_candidates, xy_constraints, yx_constraints
+    ):
+        print("+++ here!")
+        ins, outs = neuron_db.input_output_partners_with_synapse_counts()
+
+        if not xy_constraints and not yx_constraints:
+            for x_rid in x_candidates:
+                for y_rid in y_candidates - (
+                    set(ins[x_rid].keys()) | set(outs[x_rid].keys())
+                ):
+                    yield x_rid, y_rid
+            return
+
+        elif not xy_constraints:
+            for x_rid in x_candidates:
+                y_candidates_filtered = y_candidates.intersection(
+                    set(ins[x_rid].keys())
+                ) - set(outs[x_rid].keys())
+                rows = neuron_db.connections_.rows_for_cell(x_rid)
+                to_x_tuples = defaultdict(list)
+                for r in rows:
+                    if r[1] == x_rid and r[0] in y_candidates_filtered:
+                        to_x_tuples[r[0]].append(r)
+                for y_rid in y_candidates_filtered:
+                    if any(
+                        [
+                            MotifSearchQuery.row_satisfies_constraints(
+                                row, yx_constraints
+                            )
+                            for row in to_x_tuples[y_rid]
+                        ]
+                    ):
+                        yield x_rid, y_rid
+            return
+
+        elif not yx_constraints:
+            for x_rid in x_candidates:
+                y_candidates_filtered = y_candidates.intersection(
+                    set(outs[x_rid].keys())
+                ) - set(ins[x_rid].keys())
+                rows = neuron_db.connections_.rows_for_cell(x_rid)
+                from_x_tuples = defaultdict(list)
+                for r in rows:
+                    if r[0] == x_rid and r[1] in y_candidates_filtered:
+                        from_x_tuples[r[1]].append(r)
+                for y_rid in y_candidates_filtered:
+                    if any(
+                        [
+                            MotifSearchQuery.row_satisfies_constraints(
+                                row, xy_constraints
+                            )
+                            for row in from_x_tuples[y_rid]
+                        ]
+                    ):
+                        yield x_rid, y_rid
+            return
+
+        else:  # both edges are specified
+            for x_rid in x_candidates:
+                y_candidates_filtered = y_candidates.intersection(
+                    set(ins[x_rid].keys()).intersection(set(outs[x_rid]).keys())
+                )
+                rows = neuron_db.connections_.rows_for_cell(x_rid)
+                from_x_tuples = defaultdict(list)
+                to_x_tuples = defaultdict(list)
+                for r in rows:
+                    if r[0] == x_rid and r[1] in y_candidates_filtered:
+                        from_x_tuples[r[1]].append(r)
+                    elif r[1] == x_rid and r[0] in y_candidates_filtered:
+                        to_x_tuples[r[0]].append(r)
+                for y_rid in y_candidates_filtered:
+                    if any(
+                        [
+                            MotifSearchQuery.row_satisfies_constraints(
+                                row, xy_constraints
+                            )
+                            for row in from_x_tuples[y_rid]
+                        ]
+                    ) and any(
+                        [
+                            MotifSearchQuery.row_satisfies_constraints(
+                                row, yx_constraints
+                            )
+                            for row in to_x_tuples[y_rid]
+                        ]
+                    ):
+                        yield x_rid, y_rid
+            return
+
+    @staticmethod
+    def append_edge_queries(edge_constraints, from_queries, to_queries):
+        if edge_constraints:
+            if edge_constraints.regions:
+                rlist = ",".join(edge_constraints.regions)
+                from_queries.append(f"output_neuropils == {rlist}")
+                to_queries.append(f"input_neuropils == {rlist}")
+            if edge_constraints.nt_type:
+                from_queries.append(f"nt_type == {edge_constraints.nt_type}")
+
+    @staticmethod
+    def filter_by_query(neuron_db, candidates, queries):
+        if queries:
+            return candidates.intersection(set(neuron_db.search(" && ".join(queries))))
+        else:
+            return candidates
+
+    @staticmethod
     def _search_triplets(
         neuron_db,
         x,
@@ -454,62 +575,162 @@ class MotifSearchQuery(object):
         assert all(
             [isinstance(c, set) for c in [x_candidates, y_candidates, z_candidates]]
         )
-        xy_pairs = MotifSearchQuery._search_pairs(
-            neuron_db,
-            x=x,
-            x_candidates=x_candidates,
-            y=y,
-            y_candidates=y_candidates,
-            xy_edge_constraints=xy_edge_constraints,
-            yx_edge_constraints=yx_edge_constraints,
-            limit=None,
-        )
-        xz_pairs = MotifSearchQuery._search_pairs(
-            neuron_db,
-            x=x,
-            x_candidates=x_candidates,
-            y=z,
-            y_candidates=z_candidates,
-            xy_edge_constraints=xz_edge_constraints,
-            yx_edge_constraints=zx_edge_constraints,
-            limit=None,
-        )
-        yz_pairs = MotifSearchQuery._search_pairs(
-            neuron_db,
-            x=y,
-            x_candidates=y_candidates,
-            y=z,
-            y_candidates=z_candidates,
-            xy_edge_constraints=yz_edge_constraints,
-            yx_edge_constraints=zy_edge_constraints,
-            limit=None,
+        print(
+            f"+++ x, y, z: {len(x_candidates)}, {len(y_candidates)}, {len(z_candidates)}"
         )
 
-        x_ids_for_z_id = {z_id: set() for z_id in z_candidates}
-        for p in xz_pairs:
-            x_ids_for_z_id[p["nodes"][z]["id"]].add(p["nodes"][x]["id"])
+        print(f"+++ xy_ec: {xy_edge_constraints}")
+        print(f"+++ xz_ec: {xz_edge_constraints}")
+        print(f"+++ yx_ec: {yx_edge_constraints}")
+        print(f"+++ yz_ec: {yz_edge_constraints}")
+        print(f"+++ zx_ec: {zx_edge_constraints}")
+        print(f"+++ zy_ec: {zy_edge_constraints}")
 
-        y_ids_for_z_id = {z_id: set() for z_id in z_candidates}
-        for p in yz_pairs:
-            y_ids_for_z_id[p["nodes"][z]["id"]].add(p["nodes"][y]["id"])
+        x_query, y_query, z_query = [], [], []
+        MotifSearchQuery.append_edge_queries(xy_edge_constraints, x_query, y_query)
+        MotifSearchQuery.append_edge_queries(yx_edge_constraints, y_query, x_query)
+        MotifSearchQuery.append_edge_queries(xz_edge_constraints, x_query, z_query)
+        MotifSearchQuery.append_edge_queries(zx_edge_constraints, z_query, x_query)
+        MotifSearchQuery.append_edge_queries(yz_edge_constraints, y_query, z_query)
+        MotifSearchQuery.append_edge_queries(zy_edge_constraints, z_query, y_query)
+
+        x_candidates = MotifSearchQuery.filter_by_query(
+            neuron_db, x_candidates, x_query
+        )
+        y_candidates = MotifSearchQuery.filter_by_query(
+            neuron_db, y_candidates, y_query
+        )
+        z_candidates = MotifSearchQuery.filter_by_query(
+            neuron_db, z_candidates, z_query
+        )
+
+        print(
+            f"+++ x, y, z after edge queries: {len(x_candidates)}, {len(y_candidates)}, {len(z_candidates)}"
+        )
+
+        ins, outs = neuron_db.input_output_partner_sets()
+
+        def z_satisfies_constraints(x_rid, y_rid, z_rid):
+            constraints_satisfied = set()
+
+            print("+++ checking if z satisfies constraints")
+            if not xz_edge_constraints:
+                if z_rid in outs[x_rid]:
+                    print("+++ xz should not be an edge")
+                    return False
+                else:
+                    constraints_satisfied.add("xz")
+            if not zx_edge_constraints:
+                if x_rid in outs[z_rid]:
+                    print("+++ zx should not be an edge")
+                    return False
+                else:
+                    constraints_satisfied.add("zx")
+            if not yz_edge_constraints:
+                if z_rid in outs[y_rid]:
+                    print("+++ yz should not be an edge")
+                    return False
+                else:
+                    constraints_satisfied.add("yz")
+            if not zy_edge_constraints:
+                if y_rid in outs[z_rid]:
+                    print("+++ zy should not be an edge")
+                    return False
+                else:
+                    constraints_satisfied.add("zy")
+
+            if len(constraints_satisfied) == 4:
+                return True
+
+            z_rows = neuron_db.connections_.rows_for_cell(z_rid)
+            print("+++ checking if z satisfies rows")
+            for r in z_rows:
+                if (
+                    r[0] == x_rid
+                    and r[1] == z_rid
+                    and MotifSearchQuery.row_satisfies_constraints(
+                        r, xz_edge_constraints
+                    )
+                ):
+                    constraints_satisfied.add("xz")
+                    if len(constraints_satisfied) == 4:
+                        return True
+                    continue
+                if (
+                    r[0] == y_rid
+                    and r[1] == z_rid
+                    and not MotifSearchQuery.row_satisfies_constraints(
+                        r, yz_edge_constraints
+                    )
+                ):
+                    constraints_satisfied.add("yz")
+                    if len(constraints_satisfied) == 4:
+                        return True
+                    continue
+                if (
+                    r[0] == z_rid
+                    and r[1] == x_rid
+                    and not MotifSearchQuery.row_satisfies_constraints(
+                        r, zx_edge_constraints
+                    )
+                ):
+                    constraints_satisfied.add("zx")
+                    if len(constraints_satisfied) == 4:
+                        return True
+                    continue
+                if (
+                    r[0] == z_rid
+                    and r[1] == y_rid
+                    and not MotifSearchQuery.row_satisfies_constraints(
+                        r, zy_edge_constraints
+                    )
+                ):
+                    constraints_satisfied.add("zy")
+                    if len(constraints_satisfied) == 4:
+                        return True
+                    continue
+
+            return False
+
+        def filtered_z_candidates(x_rid, y_rid):
+            res = z_candidates
+            if xz_edge_constraints:
+                res = res.intersection(outs[x_rid])
+            if yz_edge_constraints:
+                res = res.intersection(outs[y_rid])
+            if zx_edge_constraints:
+                res = res.intersection(ins[x_rid])
+            if zy_edge_constraints:
+                res = res.intersection(ins[y_rid])
+            return res
 
         matches = []
-        for xy_pair in xy_pairs:
-            x_id, y_id = xy_pair["nodes"][x]["id"], xy_pair["nodes"][y]["id"]
-            if x_id == y_id:
-                continue
-            for z_id in z_candidates:
-                if z_id == x_id or z_id == y_id:
+        print("+++ starting the loop")
+        for _x_rid, _y_rid in MotifSearchQuery._feasible_pairs(
+            neuron_db=neuron_db,
+            x_candidates=x_candidates,
+            y_candidates=y_candidates,
+            xy_constraints=xy_edge_constraints,
+            yx_constraints=yx_edge_constraints,
+        ):
+            print(f"+++ examining {_x_rid}, {_y_rid}")
+
+            z_candidates_filtered = filtered_z_candidates(_x_rid, _y_rid)
+
+            for _z_rid in z_candidates_filtered:
+                if _z_rid == _x_rid or _z_rid == _y_rid:
+                    print("+++ z is equal to y or x")
                     continue
-                if x_id in x_ids_for_z_id[z_id] and y_id in y_ids_for_z_id[z_id]:
+                if z_satisfies_constraints(x_rid=_x_rid, y_rid=_y_rid, z_rid=_z_rid):
+                    print(f"+++ appending result, have {len(matches)} need {limit}")
                     matches.append(
                         MotifSearchQuery.make_match_dict(
                             neuron_db=neuron_db,
-                            nodes=[(x, x_id), (y, y_id), (z, z_id)],
+                            nodes=[(x, _x_rid), (y, _y_rid), (z, _z_rid)],
                             edges=[],
                         )
                     )
                     if len(matches) >= limit:
-                        break
+                        return matches
 
         return matches
