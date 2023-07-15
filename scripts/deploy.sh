fbfd931d40e457d7df4ec30c324e48a1c3fc9d6f
Arie Matsliah <arie.matsliah@gmail.com>
2023-07-15 16:27:56 -0400

    update meta

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ gcloud run deploy --source . \
         --set-env-vars "BUILD_TIMESTAMP=${TIMESTAMP}" \
         --region us-east1 \
         --allow-unauthenticated \
-        --min-instances 5 \
+        --min-instances 3 \
         --cpu 2 \
         --memory 8G \
         codex && \
